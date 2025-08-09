a1a81e457bba8c735a10ff234dac7c3b096e1e26
Sang-19 <sangbedpati1234@gmail.com>
2025-08-09 18:15:31 +0530

    adding the setting page

---
+++ Frontend/app/(tabs)/settings.tsx
@@ -0,0 +1,23 @@
+import {
+  Bell,
+  ChevronRight,
+  Database,
+  CircleHelp as HelpCircle,
+  Info,
+  Mail,
+  Moon,
+  Shield,
+  Smartphone,
+  Sun,
+} from "lucide-react-native";
+import { useState } from "react";
+import {
+  Alert,
+  ScrollView,
+  StyleSheet,
+  Switch,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
