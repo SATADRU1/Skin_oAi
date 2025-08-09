77e7ceab1ee0bf5d312ec488bbe44d25209b905f
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2025-08-09 16:21:02 +0530

    full updated model1.py

--- Backend/model1.py
+++ Backend/model1.py
@@ -1,3 +1,9 @@
 from roboflow import Roboflow
 
 rf = Roboflow(api_key="...................")
+project = rf.workspace().project("my-first-project-apmvj")
+model = project.version(1).model
+result = model.predict("img1.jpg").json()
+confidence=result['predictions'][0]['predictions'][0]['confidence']
+class_=result['predictions'][0]['predictions'][0]['class']
+print(class_,confidence)
