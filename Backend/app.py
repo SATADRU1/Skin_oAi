d9eca74efead27cafc5b725178d48ac27e8ae9f3
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2025-08-09 15:28:20 +0530

    Update app.py (implement Roboflow )

--- Backend/app.py
+++ Backend/app.py
@@ -6,3 +6,10 @@ import io
 import tempfile
 import os
 from roboflow import Roboflow
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+# Initialize Roboflow connection
+rf = Roboflow(api_key="K3NtQYY2EhfGyAd78fvK")
+project = rf.workspace().project("my-first-project-apmvj")
